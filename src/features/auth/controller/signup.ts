dd54570bae022590ba7c8317fe45fbff8466068d
Thanh nguyen <jonhsonle1122@gmail.com>
2023-10-11 22:16:20 +0700

    add new file

--- src/features/auth/controller/signup.ts
+++ src/features/auth/controller/signup.ts
@@ -2,9 +2,10 @@ import { ObjectId } from 'mongodb';
 import { Request, Response } from 'express';
 import { joiValidation } from '@global/decorators/joi-validation.decorators';
 import { signupSchema } from '@auth/schemes/signup';
-import { IAuthDocument } from '@auth/interfaces/auth.interface';
+import { IAuthDocument, ISignUpData } from '@auth/interfaces/auth.interface';
 import { authService } from '@service/db/auth.service';
 import { BadRequestError } from '@global/helpers/error-handler';
+import { Helpers } from '@global/helpers/helpers';
 
 export class SignUp {
     @joiValidation(signupSchema)
@@ -14,5 +15,20 @@ export class SignUp {
         if (checkIfUserExist) {
             throw new BadRequestError('Invalid credentials');
         }
+        const authObjectId: ObjectId = new ObjectId();
+        const userObjectId: ObjectId = new ObjectId();
+        const uId: ObjectId = `${Helpers.generateRandomIntegers(12)}`;
+
+    }
+    private aignupData(data: ISignUpData): IAuthDocument {
+        const { _id, username, email, password, avataColor } = data;
+        return {
+            _id,
+            uId,
+            username: Helpers.firstLetterUpperCase(username),
+            email: Helpers.lowCase(email),
+            password,
+            avatarColor, createdAt: new Date()
+        } as IAuthDocument;
     }
 }
--- src/shared/globals/helpers/helpers.ts
+++ src/shared/globals/helpers/helpers.ts
@@ -7,4 +7,14 @@ export class Helpers {
         const valueString = str.toLowerCase();
         return valueString.split('').map((value: string) => `${value.charAt(0).toUpperCase()}${value.slice(1).toUpperCase()}`).join('');
     }
+    static generateRandomIntegers(integerLength: number): number {
+        const characters = '0123456789';
+        let result = ' ';
+        const charactersLength = characters.length;
+        for (let i = 0; i < integerLength; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return parseInt(result, 10);
+    }
+
 }
